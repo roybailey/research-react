9b09c75528fa2871db800de8b13fe15e00a7ebff
Roy Bailey <roybaileybiz@gmail.com>
2016-01-22 06:57:40 +0000

    import running

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,108 @@
 var webpack = require('webpack');
+var path = require('path');
+
+console.log("Building for "+process.env.NODE_ENV);
 
 function getEntrySources(sources) {
-    if (process.env.NODE_ENV !== 'production') {
+    //if (process.env.NODE_ENV !== 'production') {
         sources.push('webpack-dev-server/client?http://localhost:8080');
         sources.push('webpack/hot/only-dev-server');
-    }
+    //}
     return sources;
 }
 
-module.exports = {
-    entry: {
-        ResearchReact: getEntrySources([
-            './src/main/js/ResearchReact.jsx'
-        ])
-    },
-    output: {
-        //publicPath: 'http://localhost:8080/',
-        filename: 'public/[name].js'
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/ },
-            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
-            { test: /\.less$/, loaders: ['style', 'css', 'less'] }
-        ]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin()
+var config = {
+    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+    //entry: {
+    //    ResearchReact: getEntrySources([
+    //        'babel-polyfill',
+    //        './src/app/App.js'
+    //    ])
+    //},
+    entry: [
+        'babel-polyfill',
+        './src/app/App.js'
     ],
+    output: {
+        path: path.join(__dirname, 'public', 'dist'),
+        filename: "bundle.js"
+    },
     resolve: {
         // you can now require('file') instead of require('file.coffee')
         extensions: ['', '.js', '.jsx', '.json']
-    }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['stage-0', 'es2015', 'react']
+                }
+            },
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.scss$/, loaders: ['style', 'css', 'sass']},
+            {test: /\.less$/, loaders: ['style', 'css', 'less']},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+            {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.gif/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: require.resolve("react"),
+                loader: "expose?React"
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./public",
+        //noInfo: true, //  --no-info option
+        inline: true,
+        colors: true,
+        hot: true,
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin()
+        , new webpack.ProvidePlugin({
+            ReactDOM: 'react-dom',
+            React: 'react'
+        })
+    ]
 };
+
+
+/*
+ * If bundling for production, optimize output
+if (process.env.NODE_ENV === 'production') {
+    config.devtool = false;
+    config.plugins = [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({comments: false}),
+        new webpack.DefinePlugin({
+            'process.env': {NODE_ENV: JSON.stringify('production')}
+        })
+    ];
+}
+ */
+
+module.exports = config;
